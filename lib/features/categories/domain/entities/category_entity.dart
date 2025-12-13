859d60232bf5ba553d91d56e40add6bac1270e2e
AsilbekDeveloper <ilhomovasilbek7@gmail.com>
2025-12-13 12:27:22 +0500

    Added Equatable

--- lib/features/categories/domain/entities/category_entity.dart
+++ lib/features/categories/domain/entities/category_entity.dart
@@ -1,17 +1,20 @@
-class CategoryEntity {
+import 'package:equatable/equatable.dart';
+
+class CategoryEntity extends Equatable {
   final String categoryId;
   final String categoryName;
   final String userId;
   final int iconCode;
-  final String? fontFamily;
   final int color;
 
-  CategoryEntity({
+  const CategoryEntity({
     required this.categoryId,
     required this.categoryName,
     required this.userId,
     required this.iconCode,
-    required this.fontFamily,
     required this.color,
   });
+
+  @override
+  List<Object?> get props => [categoryId, categoryName, userId, iconCode, color];
 }
